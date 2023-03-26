33c2443134d9f096f1ee07abe29ae68f337921d7
jyh2610 <jogg4177@naver.com>
2023-03-26 21:21:08 +0900

    Modify: eslint

--- FrontEnd/.eslintrc.js
+++ FrontEnd/.eslintrc.js
@@ -31,7 +31,6 @@ module.exports = {
         "react/jsx-no-useless-fragment": "warn", // 불필요한 fragment 금지
         "react/no-unused-state": "warn", // 사용되지 않는 state
         "react/self-closing-comp": "warn", // 셀프 클로징 태그 가능하면 적용
-        "react/jsx-curly-brace-presence": "warn", // jsx 내 불필요한 중괄호 금지
         "prettier/prettier": [
             "error",
             {
@@ -50,7 +49,6 @@ module.exports = {
         "no-unused-vars": "error",
         "@typescript-eslint/no-empty-interface": "error",
         "linebreak-style": 0,
-        "import/prefer-default-export": 0,
         "import/extensions": 0,
         "no-use-before-define": 0,
         "import/no-unresolved": 0,
