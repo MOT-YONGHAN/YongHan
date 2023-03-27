8a889ab130740c982365e2d0f260c8f44358ad1d
jyh2610 <jogg4177@naver.com>
2023-03-27 19:49:49 +0900

    wip

--- FrontEnd/src/pages/Nav/components/Login.tsx
+++ FrontEnd/src/pages/Nav/components/Login.tsx
@@ -3,10 +3,10 @@ export default function Login() {
         <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 ">
             <div className="border-2 border-gray-200">
                 <div>로그인</div>
-                <div className="flex flex-col">
+                <form className="flex flex-col">
                     <input type="text" placeholder="아이디" />
-                    <input type="text" placeholder="비밀번호" />
-                </div>
+                    <input type="password" placeholder="비밀번호" />
+                </form>
                 <div>
                     <label htmlFor="save">
                         <input type="checkbox" id="save" />
