5b9f0d4ca701611083517ede9b84ab49e4abdc84
jyh2610 <jogg4177@naver.com>
2023-03-27 17:06:40 +0900

    Modify: lint 룰 완화

--- FrontEnd/.eslintrc.js
+++ FrontEnd/.eslintrc.js
@@ -22,13 +22,7 @@ module.exports = {
         "plugin:prettier/recommended",
     ],
     rules: {
-        "no-var": "warn", // var 금지
-        "no-multiple-empty-lines": "warn", // 여러 줄 공백 금지
-        eqeqeq: "warn", // 일치 연산자 사용 필수
-        "dot-notation": "warn", // 가능하다면 dot notation 사용
-        "no-unused-vars": "warn", // 사용하지 않는 변수 금지
-        "react/jsx-pascal-case": "warn", // 컴포넌트 이름은 PascalCase로
-        "react/jsx-no-useless-fragment": "warn", // 불필요한 fragment 금지
+        "import/prefer-default-export": ["off"],
         "react/no-unused-state": "warn", // 사용되지 않는 state
         "react/self-closing-comp": "warn", // 셀프 클로징 태그 가능하면 적용
         "prettier/prettier": [
@@ -37,13 +31,10 @@ module.exports = {
                 endOfLine: "auto",
             },
         ],
-        "react/jsx-props-no-spreading": "error",
         "react/prop-types": "error",
         "react-hooks/rules-of-hooks": "error",
         "react-hooks/exhaustive-deps": "error",
-        "import/prefer-default-export": "error",
         "no-unused-vars": "error",
-        "@typescript-eslint/no-empty-interface": "error",
         "linebreak-style": 0,
         "import/extensions": 0,
         "no-use-before-define": 0,
@@ -59,6 +50,9 @@ module.exports = {
         "@typescript-eslint/explicit-module-boundary-types": 0,
     },
     settings: {
+        react: {
+            version: "detect",
+        },
         "import/resolver": {
             node: {
                 extensions: [".js", ".jsx", ".ts", ".tsx"],
@@ -66,3 +60,12 @@ module.exports = {
         },
     },
 };
+// "no-var": "warn", // var 금지
+// "no-multiple-empty-lines": "warn", // 여러 줄 공백 금지
+// eqeqeq: "warn", // 일치 연산자 사용 필수
+// "dot-notation": "warn", // 가능하다면 dot notation 사용
+// "no-unused-vars": "warn", // 사용하지 않는 변수 금지
+// "react/jsx-pascal-case": "warn", // 컴포넌트 이름은 PascalCase로
+// "react/jsx-no-useless-fragment": "warn", // 불필요한 fragment 금지
+// "react/jsx-props-no-spreading": "error",
+//"@typescript-eslint/no-empty-interface": "error",
--- FrontEnd/src/pages/Nav/Nav.tsx
+++ FrontEnd/src/pages/Nav/Nav.tsx
@@ -1,14 +1,19 @@
+import { RxMagnifyingGlass } from "react-icons/rx";
+
 function Nav() {
     return (
-        <div className="">
-            <div>
+        <div className="flex justify-between mt-3 ml-4 ">
+            <div className="flex justify-between w-[1200px]">
                 <span>YOUNG HAN</span>
+                <div className="w-44 flex justify-between">
+                    <button type="button">오늘의 운세</button>
+                    <button type="button">최근 본 집</button>
+                </div>
             </div>
-            <div>
-                <span>오늘의 운세</span>
-                <span>최근 본 집</span>
+            <div className="flex justify-between mr-4 w-20">
+                <button type="button">로그인</button>
+                <RxMagnifyingGlass size={25} />
             </div>
-            <div>로그인</div>
         </div>
     );
 }
