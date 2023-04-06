3274d9a36766f0403033443bd8d84ea554c3cdee
jyh2610 <jogg4177@naver.com>
2023-04-06 16:16:20 +0900

    ADD: change  kakao login

--- FrontEnd/src/pages/Nav/components/soicallogin/KakaoLogout.tsx
+++ FrontEnd/src/pages/Nav/components/soicallogin/KakaoLogout.tsx
@@ -3,16 +3,16 @@ import { useEffect } from "react";
 const { Kakao } = window;
 
 function KakaoLogout() {
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://developers.kakao.com/sdk/js/kakao.js";
-        script.async = true;
-        document.body.appendChild(script);
+    // useEffect(() => {
+    //     const script = document.createElement("script");
+    //     script.src = "https://developers.kakao.com/sdk/js/kakao.js";
+    //     script.async = true;
+    //     document.body.appendChild(script);
 
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, []);
+    //     return () => {
+    //         document.body.removeChild(script);
+    //     };
+    // }, []);
     const handleKakaoLogout = () => {
         if (window.Kakao) {
             // Kakao 객체가 로드되었는지 확인합니다.
