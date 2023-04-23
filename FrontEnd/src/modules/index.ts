62803cd70c972d380c0333abb607b100de22568c
Jangchan0 <jchy16@gmail.com>
2023-04-24 00:21:19 +0900

    MODIFY: detailReducer 추가

--- FrontEnd/src/modules/index.ts
+++ FrontEnd/src/modules/index.ts
@@ -2,8 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import formReducer from "./form";
 import modalReducer from "./LoginModal";
 import searchReducer from "./search";
+import detailReducer from "./detail";
 
 const store = configureStore({
-    reducer: { formReducer, modalReducer, searchReducer },
+    reducer: { formReducer, modalReducer, searchReducer, detailReducer },
 });
 export default store;
